2195ce00ed59b8958bf7d4491420ab95e8ada972
Viraj Chavan <virajc@live.com>
2020-12-17 11:33:51 +0530

    Remove reduntant routes and controller methods

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,42 +1,9 @@
 class NotesController < ApiController
   before_action :authenticate_request!
-  before_action :set_note, only: [:show, :update, :destroy]
 
   # GET /notes
   def index
-    @notes = Note.root_notes_json(@current_user.id)
-
-    render json: @notes
-  end
-
-  # GET /notes/1
-  def show
-    render json: @note
-  end
-
-  # POST /notes
-  def create
-    @note = Note.new(note_params)
-    @note.user = @current_user
-    if @note.save
-      render json: @note, status: :created
-    else
-      render json: @note.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /notes/1
-  def update
-    if @note.update(note_params)
-      render json: @note
-    else
-      render json: @note.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /notes/1
-  def destroy
-    @note.destroy
+    render json: Note.root_notes_json(@current_user.id)
   end
 
   # POST /notes/process_transactions
@@ -54,14 +21,4 @@ class NotesController < ApiController
     render json: { status: "success", new_ids: new_note_ids }
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_note
-      @note = Note.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def note_params
-      params.require(:note).permit(:content, :path, :order)
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   scope '/api' do
-    resources :notes do
+    resources :notes, only: [:index] do
       collection do
         post 'process_transactions'
       end
