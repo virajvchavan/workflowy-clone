c7c84f25f54a8c2d7a5b631ed070b454b4ada921
Viraj Chavan <virajc@live.com>
2020-12-18 22:12:42 +0530

    Add intial notes to the user when an account is created

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,25 @@ class User
   validates :email, presence: true
   index({ email: 1 }, { unique: true, name: 'email_index' })
 
+  after_create :add_initial_notes
+
   def id
     _id.to_s
+  end
+
+  def add_initial_notes
+    path = File.join(Rails.root, "app/seeds/initialNotes.json")
+    new_notes = JSON.parse(File.read(path))
+    import_notes_from_json(new_notes, "/", nil)
+  end
+
+  def import_notes_from_json(new_notes, path, parent)
+    path = path + "#{parent.id.to_s}/" if parent
+    new_notes.each_with_index do |note_data, index|
+      added_note = self.notes.create(content: note_data['content'], collapsed: note_data['collapsed'], path: path, order: index)
+      if (note_data['child_notes'] && note_data['child_notes'].length > 0)
+        import_notes_from_json(note_data['child_notes'], path, added_note)
+      end
+    end
   end
 end
